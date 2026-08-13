5106b66bae34f9d55824287bb3da9e7308deb782
chenglujiang46-cyber <chenglujiang46@gmail.com>
2026-08-13 19:02:09 +0800

    fix: add modules/tools __init__.py (export convert_cookies helpers)

--- modules/tools/__init__.py
+++ modules/tools/__init__.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Utility package for tools under modules/tools.
 """
-
-__all__ = ["convert_cookies"]
+Minimal __init__ to expose convert_cookies helpers for imports.
+"""
+from .convert_cookies import (
+    is_netscape_file,
+    convert_any_to_netscape,
+    _iso8601_to_epoch as iso8601_to_epoch,
+)
